aa3a35e3d38c7ff06c6291bf19aa1076d9b13d6f
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-02-15 20:39:34 +0530

    style: added footer

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"`)
+    console.log(audio)
+    console.log(key)
     if(!audio) return; //stops the function from running
     audio.currentTime = 0; //rewinds to the starting
     audio.play()
     key.classList.add("playing");
     }
-function removeTransiition(e){
+function removeTransition(e){
     if(e.propertyName !== 'transform') return; //skip if not transformed
     this.classList.remove("playing")
     }
   
 const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener('transitionend', removeTransiition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   
 window.addEventListener('keydown', playSound);
