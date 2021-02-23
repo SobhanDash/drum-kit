5876012aaeb93de327303773bc4cd29b8e7ab5fb
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-02-23 21:38:21 +0530

    test: make mobile responsive

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    const key = document.querySelector(`.key[data-key="${e.code}"`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.keys[data-key="${e.keyCode}"`)
     console.log(audio)
     console.log(key)
+    
     if(!audio) return; //stops the function from running
+
     audio.currentTime = 0; //rewinds to the starting
     audio.play()
     key.classList.add("playing");
     }
 function removeTransition(e){
     if(e.propertyName !== 'transform') return; //skip if not transformed
+    console.log(this.classList)
     this.classList.remove("playing")
+    console.log("running")
     }
   
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keyss = document.querySelectorAll(".keys");
+keyss.forEach(key => key.addEventListener('transitionend', removeTransition));
   
 window.addEventListener('keydown', playSound);
